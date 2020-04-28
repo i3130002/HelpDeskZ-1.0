2c28118b1b638e7f3af4032aad08241d43bd8067
i3130002 <i3130002@gmail.com>
2020-04-28 14:39:18 +0430

    FIX Security issue #45
    This might fix the issue as it should prevent moving file from temp directory to uploads before verifying thus preventing execution! 
    I did not verify the code so please recheck & run & test the code.

--- controllers/submit_ticket_controller.php
+++ controllers/submit_ticket_controller.php
@@ -141,25 +141,26 @@ if($action == 'displayForm' || $action == 'confirmation'){
 						$filename = md5($_FILES['attachment']['name'].time()).".".$ext;
 						$fileuploaded[] = array('name' => $_FILES['attachment']['name'], 'enc' => $filename, 'size' => formatBytes($_FILES['attachment']['size']), 'filetype' => $_FILES['attachment']['type']);
 						$uploadedfile = $uploaddir.$filename;
-						if (!move_uploaded_file($_FILES['attachment']['tmp_name'], $uploadedfile)) {
+						$fileverification = verifyAttachment($_FILES['attachment']);
+						switch($fileverification['msg_code']){
+							case '1':
 							$show_step2 = true;
-							$error_msg = $LANG['ERROR_UPLOADING_A_FILE'];
-						}else{
-							$fileverification = verifyAttachment($_FILES['attachment']);
-							switch($fileverification['msg_code']){
-								case '1':
-								$show_step2 = true;
-								$error_msg = $LANG['INVALID_FILE_EXTENSION'];
-								break;
-								case '2':
-								$show_step2 = true;
-								$error_msg = $LANG['FILE_NOT_ALLOWED'];
-								break;
-								case '3':
-								$show_step2 = true;
-								$error_msg = str_replace('%size%',$fileverification['msg_extra'],$LANG['FILE_IS_BIG']);
-								break;
+							$error_msg = $LANG['INVALID_FILE_EXTENSION'];
+							break;
+							case '2':
+							$show_step2 = true;
+							$error_msg = $LANG['FILE_NOT_ALLOWED'];
+							break;
+							case '3':
+							$show_step2 = true;
+							$error_msg = str_replace('%size%',$fileverification['msg_extra'],$LANG['FILE_IS_BIG']);
+							break;
+							case '0':
+							if (!move_uploaded_file($_FILES['attachment']['tmp_name'], $uploadedfile)) {
+							    $show_step2 = true;
+							    $error_msg = $LANG['ERROR_UPLOADING_A_FILE'];
 							}
+							break;
 						}
 					}	
 				}
