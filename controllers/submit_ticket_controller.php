be78811b773b201957fbc8e599c9256fc2ac0053
i3130002 <i3130002@gmail.com>
2020-04-28 14:34:42 +0430

    FIX for "Security issue I found online #45 "
    This might fix the issue as it should prevent moving file from temp directory to uploads before verifying thus preventing execution! 
    I did not verify the code so please recheck & run & test the code.
    Should be flagged as Security vulnerability.
    Thanks to 
    tamkeen-tms to submit the issue #45 
    John Hammond by showing how exploit works (https://www.youtube.com/watch?v=axxCpCTmu_k)
    0xRick https://0xrick.github.io/hack-the-box/help/

--- controllers/submit_ticket_controller.php
+++ controllers/submit_ticket_controller.php
@@ -140,12 +140,8 @@ if($action == 'displayForm' || $action == 'confirmation'){
 						$ext = pathinfo($_FILES['attachment']['name'], PATHINFO_EXTENSION);
 						$filename = md5($_FILES['attachment']['name'].time()).".".$ext;
 						$fileuploaded[] = array('name' => $_FILES['attachment']['name'], 'enc' => $filename, 'size' => formatBytes($_FILES['attachment']['size']), 'filetype' => $_FILES['attachment']['type']);
-						$uploadedfile = $uploaddir.$filename;
-						if (!move_uploaded_file($_FILES['attachment']['tmp_name'], $uploadedfile)) {
-							$show_step2 = true;
-							$error_msg = $LANG['ERROR_UPLOADING_A_FILE'];
-						}else{
-							$fileverification = verifyAttachment($_FILES['attachment']);
+                        $uploadedfile = $uploaddir.$filename;
+                        $fileverification = verifyAttachment($_FILES['attachment']);
 							switch($fileverification['msg_code']){
 								case '1':
 								$show_step2 = true;
@@ -159,8 +155,13 @@ if($action == 'displayForm' || $action == 'confirmation'){
 								$show_step2 = true;
 								$error_msg = str_replace('%size%',$fileverification['msg_extra'],$LANG['FILE_IS_BIG']);
 								break;
+								case '0':
+								if (!move_uploaded_file($_FILES['attachment']['tmp_name'], $uploadedfile)) {
+								    $show_step2 = true;
+								    $error_msg = $LANG['ERROR_UPLOADING_A_FILE'];
+								}
+								break;
 							}
-						}
 					}	
 				}
 				
